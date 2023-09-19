4b7bf8bf4b8530b2b0f0481e18720adf00c15b16
adamgonda <adamgondagyula@gmail.com>
2023-09-19 20:36:53 +0200

    add initial next setup, and routes

---
+++ cooltix-homework/apps/cooltix-homework/pages/index.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledPage = styled.div`
+  .page {
+  }
+`;
+
+export function Index() {
+  return (
+    <h1>hello</h1>
+  );
+}
+
+export default Index;
