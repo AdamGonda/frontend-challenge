4e6094f2962e2a16272082a17ccd1a9434ee8573
adamgonda <adamgondagyula@gmail.com>
2023-09-25 07:15:13 +0200

    make logo navigate to index

--- apps/cooltix-homework/components/Header.tsx
+++ apps/cooltix-homework/components/Header.tsx
@@ -3,19 +3,22 @@ import SearchBar from './SearchBar';
 import Image from 'next/image';
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 function Navigation() {
   const router = useRouter();
 
   return (
     <Header>
-      <Image
-        priority
-        src="/images/logo.svg"
-        width="145"
-        height="48"
-        alt="logo"
-      />
+      <Link href="/">
+        <Image
+          priority
+          src="/images/logo.svg"
+          width="145"
+          height="48"
+          alt="logo"
+        />
+      </Link>
       {router.pathname === '/' && <SearchBar />}
     </Header>
   );
